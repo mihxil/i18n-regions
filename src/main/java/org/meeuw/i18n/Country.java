7198e2d130420c4bee7c16be88c471da20e7b07f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-06-11 13:12:51 +0200

    Rationalized provided predicates a bit.

--- src/main/java/org/meeuw/i18n/Country.java
+++ src/main/java/org/meeuw/i18n/Country.java
@@ -12,9 +12,9 @@ import com.neovisionaries.i18n.CountryCode;
  */
 public interface Country extends Region {
 
-    Predicate<Region> CURRENT_OFFICIAL = (c) -> c instanceof CurrentCountry && ((CurrentCountry) c).getAssignment() == CountryCode.Assignment.OFFICIALLY_ASSIGNED;
-    Predicate<Region> CURRENT_AND_FORMER = CURRENT_OFFICIAL.or(c -> c instanceof FormerCountry);
-    Predicate<Region> CURRENT_AND_FORMER_AND_USER = CURRENT_AND_FORMER.or(c -> c instanceof UserAssignedCountry);
+    Predicate<Region> OFFICIAL = (c) -> c instanceof CurrentCountry && ((CurrentCountry) c).getAssignment() == CountryCode.Assignment.OFFICIALLY_ASSIGNED;
+    Predicate<Region> IS_FORMER = c -> c instanceof FormerCountry;
+    Predicate<Region> IS_USER = c -> c instanceof UserAssignedCountry;
 
     static Optional<Country> getByCode(String code) {
         return Regions.getByCode(code, Country.class);
