34afe4af5e15f537fc6cef9481b433da48445fa6
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-06-18 16:16:19 +0200

    Reindented.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,37 +5,37 @@ import org.meeuw.i18n.subdivisions.CountrySubdivisionProvider;
 import org.meeuw.i18n.subdivisions.UserAssignedCountrySubdivisionProvider;
 
 module org.meeuw.i18n {
-	exports org.meeuw.i18n;
-	exports org.meeuw.i18n.bind.jaxb;
-	exports org.meeuw.i18n.continents;
-	exports org.meeuw.i18n.countries;
-	exports org.meeuw.i18n.persistence;
-	exports org.meeuw.i18n.subdivisions;
-	exports org.meeuw.i18n.validation;
-	exports org.meeuw.i18n.spi;
+    exports org.meeuw.i18n;
+    exports org.meeuw.i18n.bind.jaxb;
+    exports org.meeuw.i18n.continents;
+    exports org.meeuw.i18n.countries;
+    exports org.meeuw.i18n.persistence;
+    exports org.meeuw.i18n.subdivisions;
+    exports org.meeuw.i18n.validation;
+    exports org.meeuw.i18n.spi;
 
 
-	requires transitive nv.i18n;
-	requires transitive i18n.subdivisions;
-	requires transitive org.meeuw.i18n.formerlyassigned;
+    requires transitive nv.i18n;
+    requires transitive i18n.subdivisions;
+    requires transitive org.meeuw.i18n.formerlyassigned;
 
-	requires static java.validation;
+    requires static java.validation;
     requires static java.xml.bind;
-	requires static org.checkerframework.checker.qual;
-	requires static java.persistence;
+    requires static org.checkerframework.checker.qual;
+    requires static java.persistence;
 
-	provides RegionProvider with
+    provides RegionProvider with
 
-		CurrentCountryProvider,
-		FormerCountryProvider,
+        CurrentCountryProvider,
+        FormerCountryProvider,
 
-		UserAssignedProvider,
-		UserAssignedCountrySubdivisionProvider,
-		UnofficialCurrentCountryProvider,
-		VehicleRegistrationCodeFallbackProvider,
+        UserAssignedProvider,
+        UserAssignedCountrySubdivisionProvider,
+        UnofficialCurrentCountryProvider,
+        VehicleRegistrationCodeFallbackProvider,
 
-		CountrySubdivisionProvider,
+        CountrySubdivisionProvider,
 
-		ContinentProvider;
+        ContinentProvider;
 
 }
