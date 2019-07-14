0b2e201ab43b3990f73d306401b56266a46d61ab
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-07-14 10:29:45 +0200

    Limit the number of open-locations a bit.

--- tests/springboot/src/main/java/org/meeuw/springboot8/Application.java
+++ tests/springboot/src/main/java/org/meeuw/springboot8/Application.java
@@ -1,11 +1,13 @@
 package org.meeuw.springboot8;
 
+import org.meeuw.i18n.openlocationcode.OpenLocationProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackageClasses = org.meeuw.springutils.RegionsController.class)
 public class Application {
     public static void main(String[] args) {
+        OpenLocationProvider.setMaxLength(1);
         SpringApplication.run(Application.class, args);
     }
 }
