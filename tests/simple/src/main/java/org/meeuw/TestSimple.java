4209e330a7799ef70d8640255f1d8db5263926ed
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-12-17 23:32:09 +0100

    tabs to spaces

--- tests/simple/src/main/java/org/meeuw/TestSimple.java
+++ tests/simple/src/main/java/org/meeuw/TestSimple.java
@@ -12,25 +12,25 @@ import java.util.Locale;
  */
 public class TestSimple {
 
-	public static void main(String[] argv) {
-		String arg1 = argv.length == 0 ? null : argv[0];
-		Locale locale = arg1 == null ? Locale.getDefault() : new Locale(arg1);
-		RegionService.getInstance().values().forEach(v -> {
+    public static void main(String[] argv) {
+        String arg1 = argv.length == 0 ? null : argv[0];
+        Locale locale = arg1 == null ? Locale.getDefault() : new Locale(arg1);
+        RegionService.getInstance().values().forEach(v -> {
 
-			System.out.println(" " + v + ":" + v.getName() + ":" + v.getName(locale));
-			System.out.println("class: " + v.getClass().getSimpleName());
-			for (Annotation a : v.getClass().getAnnotations()) {
-				System.out.println(("annotation : " + a));
-			}/*
-			ClassUtils.getAllInterfaces(v.getClass()).forEach(c -> {
-				System.out.println("interface: " + c.getSimpleName());
-						for (Annotation a : c.getAnnotations()) {
-							// Java 11 simply won't see the annotation if the dependency is missing. Good.
-							System.out.println(("   annotation : " + a));
-						}
-					}
-			);*/
+            System.out.println(" " + v + ":" + v.getName() + ":" + v.getName(locale));
+            System.out.println("class: " + v.getClass().getSimpleName());
+            for (Annotation a : v.getClass().getAnnotations()) {
+                System.out.println(("annotation : " + a));
+            }/*
+            ClassUtils.getAllInterfaces(v.getClass()).forEach(c -> {
+                System.out.println("interface: " + c.getSimpleName());
+                        for (Annotation a : c.getAnnotations()) {
+                            // Java 11 simply won't see the annotation if the dependency is missing. Good.
+                            System.out.println(("   annotation : " + a));
+                        }
+                    }
+            );*/
 
-		});
-	}
+        });
+    }
 }
