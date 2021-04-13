6d22137508ac462a7918f4f4186996e9c97a1099
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-04-13 10:48:22 +0200

    typo

--- release.sh
+++ release.sh
@@ -18,7 +18,7 @@ DEVELOPMENT_VERSION=`echo $SNAPSHOT_VERSION | awk -F[.-] '{print $1"."$2+1"-"$3}
 BRANCH_DEVELOPMENT_VERSION=`echo $TARGET_VERSION | awk -F[.-] '{print $1"."$2"."$3+1"-SNAPSHOT"}'`
 echo "$SNAPSHOT_VERSION -> $DEVELOPMENT_VERSION, $TARGET_VERSION, $BRANCH_DEVELOPMENT_VERSION"
 mvn release:branch -DbranchName=$SNAPSHOT_VERSION -DdevelopmentVersion=$DEVELOPMENT_VERSION
-git checkout SNAPSHOT_VERSION
+git checkout $SNAPSHOT_VERSION
 git branch -l
 echo mvn -Pdeploy release:prepare release:perform -DreleaseVersion=$TARGET_VERSION -DdevelopmentVersion=1.1.1-SNAPSHOT
 
