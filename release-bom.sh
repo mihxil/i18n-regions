70898bf0bb69855451dea2269ea2008133ee3207
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-04-13 20:14:18 +0200

    Prepared for next cycle

--- release-bom.sh
+++ release-bom.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
 
 NS=http://maven.apache.org/POM/4.0.0
+
+# Determin version of bom to be released
+
+# get from pom.xml
 BOM_VERSION=`xmllint  --noout  --shell   i18n-regions-bom/pom.xml << EOF |  grep -E -v '^(\/|text).*' | sed s/-SNAPSHOT/.0/
  setns x=$NS
  cd //x:project/x:version/text()
  cat .
 EOF
 `
-
+# ask user
 read -p "bom version ($BOM_VERSION): " user_bom
 echo $user_bom
 if [ ! -z "$user_bom" ] ; then
   BOM_VERSION=$user_bom
 fi
+
+# determin version of project to be released
 VERSION=${BOM_VERSION}
+
+# ask user
 read -p "version ($VERSION): " user_version
 if [ ! -z "$user_version" ] ; then
   VERSION=$user_version
 fi
 echo $BOM_VERSION / $VERSION
+
+# found now fill that in the bom's pom.xml
 xmllint --shell i18n-regions-bom/pom.xml << EOF > /dev/null
 setns x=$NS
 cd /x:project/x:version
@@ -28,9 +38,10 @@ set ${VERSION}
 save
 EOF
 
+# show what happens
 git diff  i18n-regions-bom/pom.xml
 
-
+# update version of bom in dependency managment itself
 xmllint --shell pom.xml << EOF > /dev/null
 setns x=$NS
 cd /x:project/x:dependencyManagement/x:dependencies/x:dependency[x:artifactId = 'i18n-regions-bom']/x:version
