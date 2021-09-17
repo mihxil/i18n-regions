9f6f831a5dc989c5e7b973ed3a115153cec47690
Michiel Meeuwissen <git@meeuw.org>
2021-09-17 09:10:29 +0200

    Improved UserAssignedCountry. Make sure als corresponding intance of CountryCode are wrapped. Added a test. Javadoc.

--- i18n-regions-countries/src/main/java/org/meeuw/i18n/countries/UserAssignedCountry.java
+++ i18n-regions-countries/src/main/java/org/meeuw/i18n/countries/UserAssignedCountry.java
@@ -3,7 +3,13 @@ package org.meeuw.i18n.countries;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.meeuw.i18n.regions.UserAssignedRegion;
 
+import com.neovisionaries.i18n.CountryCode;
+
 /**
+ * Wraps all 'User defined' {@link CountryCode}, assigns to each a {@link #getAssignedBy()}.
+ *
+ * Adds a few others.
+ *
  * http://en.wikipedia.org/wiki/ISO_3166-2
  * <a href="https://docs.google.com/spreadsheet/ccc?key=0Ajm-SImXl8LzdGVkU3ZJRV9JS3ZIVDZNNTd6eTVLSGc&amp;usp=sharing#gid=0">this doc</a>
  * @author Michiel Meeuwissen
@@ -12,31 +18,52 @@ import org.meeuw.i18n.regions.UserAssignedRegion;
 @SuppressWarnings("unused")
 public class UserAssignedCountry extends UserAssignedRegion implements Country {
 
-    public static UserAssignedCountry XZ =
-        new UserAssignedCountry("XZ", "International Waters", null, -1, "UN/LOCODE");
-    public static UserAssignedCountry QO =
-        new UserAssignedCountry("QO", "Outlying Oceania", null, -1, "By  Unicode Common Locale Data Repository");
-
-    public static UserAssignedCountry QU =
-        new UserAssignedCountry("QU", "European Union", null, -1, "Unicode Common Locale Data Repository");
-    public static final UserAssignedCountry ZZ =
-        new UserAssignedCountry("ZZ", "Unknown or Invalid Territory", null, -1, "Unicode Common Locale Data Repository");
-    public static UserAssignedCountry XK = new UserAssignedCountry("XK", "Kosovo", "XKX", -1, "European Union");
-    public static UserAssignedCountry XN = new UserAssignedCountry("XN", "Nordic Patent Institute", null, -1, "World Intellectual Property Organization");
+    public static final String ASSIGNER_UNICODE = "Unicode Common Locale Data Repository";
+    public static final String ASSIGNER_EU     = "European Union";
+    public static final String ASSIGNER_WIPO = "World Intellectual Property Organization";
+    public static final String ASSIGNER_UN = "UN/LOCODE";
 
 
+    public static final UserAssignedCountry XZ = new UserAssignedCountry("XZ", "International Waters",     ASSIGNER_UN);
+    public static final UserAssignedCountry QO = new UserAssignedCountry("QO", "Outlying Oceania",         ASSIGNER_UNICODE);
+    public static final UserAssignedCountry QU = new UserAssignedCountry("QU", "European Union", null, -1, ASSIGNER_UNICODE, CountryCode.EU);
+    public static final UserAssignedCountry ZZ = new UserAssignedCountry("ZZ", "Unknown or Invalid Territory", ASSIGNER_UNICODE);
+    public static final UserAssignedCountry XN = new UserAssignedCountry("XN", "Nordic Patent Institute",      ASSIGNER_WIPO);
+
+    public static final UserAssignedCountry XK = new UserAssignedCountry(CountryCode.XK,  ASSIGNER_EU);
+    public static final UserAssignedCountry XI = new UserAssignedCountry(CountryCode.XI, ASSIGNER_EU);
+    public static final UserAssignedCountry XU = new UserAssignedCountry(CountryCode.XU, ASSIGNER_EU);
 
     private final String alpha3;
 
     private final int number;
 
-    public UserAssignedCountry(
+    private final CountryCode countryCode;
+
+    protected UserAssignedCountry(
         @NonNull String code,
-        @NonNull String name, String alpha3, int number, String assignedBy) {
+        @NonNull String name,
+        String assignedBy) {
+        this(code, name, null, -1, assignedBy, CountryCode.UNDEFINED);
+    }
+
+    protected UserAssignedCountry(
+        @NonNull String code,
+        @NonNull String name,
+        String alpha3,
+        int number,
+        String assignedBy,
+        CountryCode countryCode) {
         super(code, name, assignedBy);
         this.alpha3 = alpha3;
         this.number = number;
+        this.countryCode = countryCode;
     }
+
+    protected UserAssignedCountry(CountryCode code, String assignedBy) {
+        this(code.getAlpha2(), code.getName(), code.getAlpha3(), code.getNumeric(), assignedBy, code);
+    }
+
 
     public String getAlpha2() {
         return code;
@@ -52,8 +79,8 @@ public class UserAssignedCountry extends UserAssignedRegion implements Country {
     }
 
     @Override
-    public String getCountryCode() {
-        return code;
+    public CountryCode getCountryCode() {
+        return countryCode;
     }
 
     @Override
---
+++ i18n-regions-countries/src/test/java/org/meeuw/i18n/countries/UserAssignedCountryProviderTest.java
@@ -0,0 +1,40 @@
+package org.meeuw.i18n.countries;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import com.neovisionaries.i18n.CountryCode;
+import com.neovisionaries.i18n.LanguageCode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserAssignedCountryProviderTest {
+    final UserAssignedCountryProvider inst = new UserAssignedCountryProvider();
+
+
+    /**
+     * Tests whether all User assigned countries in {@link CountryCode} have a corresponding instance.
+     *
+     * The only exception is {@link CountryCode#UNDEFINED} (We suggest {@link UserAssignedCountry#ZZ}
+     *
+     * Also tests that all {@link UserAssignedCountry}s are working without causing exceptions.
+     */
+    @Test
+    void values() {
+        Set<CountryCode> collect = Arrays.stream(CountryCode.values())
+            .filter(a -> a.getAssignment() == CountryCode.Assignment.USER_ASSIGNED)
+            .collect(Collectors.toSet());
+        collect.remove(CountryCode.UNDEFINED);
+        inst.values().forEach(country -> {
+            System.out.println("" + country + " " + ((Country) country).getCountryCode() + " " + country.getName() + " " + country.getName(LanguageCode.nl));
+            collect.removeIf(c -> c.equals(((Country) country).getCountryCode()));
+        });
+        assertThat(collect).isEmpty();;
+
+
+
+    }
+}
