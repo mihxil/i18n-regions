53948962eeddd47f0d57b22cf384fbff48f5d0db
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-01-16 17:19:21 +0100

    javadoc.

---
+++ i18n-regions-countries/src/main/java/org/meeuw/i18n/countries/codes/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package can contain enums. Currently, it contains a copy from the enum of nv-i18n, so that the dependency on that library can be removed.
+ * @see org.meeuw.i18n.countries.codes.CountryCode
+ */
+package org.meeuw.i18n.countries.codes;
---
+++ i18n-regions-countries/src/main/java/org/meeuw/i18n/countries/validation/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Validation of country codes using jakarta validation API.
+ * @see org.meeuw.i18n.countries.validation.ValidCountry
+ */
+package org.meeuw.i18n.countries.validation;
